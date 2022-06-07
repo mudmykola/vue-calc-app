4f0de93e89104475dde4e44f979a04df5b974e50
Mykola <46628437+mudmykola@users.noreply.github.com>
2022-06-07 23:59:49 +0300

    done=> calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,4 +1,29 @@
-<template><div>Hello Calculator</div></template>
+<template>
+  <div
+    class="p-3"
+    style="max-width: 600px; margin: 50px auto; background: #234"
+  >
+    <div
+      class="w-full rounded text-end m-1 p-3 lead font-weight-bold text-white bg-vue-dark"
+    >
+      {{ calcValue || 0 }}
+    </div>
+    <!-- Calc buttons -->
+    <div class="row no-gutters">
+      <div class="col-3" v-for="num in calcElement" :key="num">
+        <div
+          class="lead text-white text-center m-2 py-4 rounded hover-class bg-vue-dark"
+          :class="{
+            'bg-vue-green': ['C', '*', '/', '-', '+', '%', '='].includes(num),
+          }"
+          @click="action(num)"
+        >
+          {{ num }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
@@ -6,7 +31,66 @@ export default {
   props: {
     msg: String,
   },
+  data() {
+    return {
+      calcValue: "",
+      calcElement: [
+        "C",
+        "*",
+        "/",
+        "-",
+        7,
+        8,
+        9,
+        "+",
+        4,
+        5,
+        6,
+        "%",
+        1,
+        2,
+        3,
+        "=",
+        0,
+        ".",
+      ],
+      oper: null,
+      prevCalcValue: "",
+    };
+  },
+  methods: {
+    action(num) {
+      if (!isNaN(num) || num === ".") {
+        this.calcValue += num + "";
+      }
+      // Clear option
+      if (num === "C") {
+        this.calcValue = "";
+      }
+      if (num === "%") {
+        this.calcValue = this.calcValue / 100 + "";
+      }
+      if (["/", "+", "-", "*"].includes(num)) {
+        this.oper = num;
+        this.prevCalcValue = this.calcValue;
+        this.calcValue = "";
+      }
+      if (num === "=") {
+        this.calcValue = eval(this.prevCalcValue + this.oper + this.calcValue);
+        (this.prevCalcValue = ""), (this.oper = null);
+      }
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+
+.hover-class:hover {
+  cursor: pointer;
+  background: #3d5875;
+}
+.bg-vue-green {
+  background: #3fb984;
+}
+</style>
