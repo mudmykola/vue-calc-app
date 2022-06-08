b867c1516573cb05d30c8b4668c222c66628746e
Mykola <46628437+mudmykola@users.noreply.github.com>
2022-06-08 19:31:27 +0300

    Delete src directory

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-// eslint-disable-next-line prettier/prettier
-createApp(App).mount("#app");
